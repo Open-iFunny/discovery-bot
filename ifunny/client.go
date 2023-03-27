f2a3cc0bcc320b589c818536039cace7b880f627
Eva <mail@gastrodon.io>
2023-03-26 20:30:24 -0400

    move chat client code -> client

--- ifunny/client.go
+++ ifunny/client.go
@@ -1,13 +1,20 @@
 package ifunny
 
 import (
+	"fmt"
 	"io"
 	"net/http"
+
+	"github.com/jcelliott/turnpike"
+	"github.com/mitchellh/mapstructure"
 )
 
 const (
 	apiRoot   = "https://api.ifunny.mobi/v4"
 	projectID = "iFunny"
+
+	chatRoot      = "wss://chat.ifunny.co/chat"
+	chatNamespace = "co.fun.chat"
 )
 
 type Client interface {
@@ -38,4 +45,70 @@ func (client *staticClient) Request(method, path string, body io.Reader) (*http.
 
 func (client *staticClient) Connect(bearer string) (Chat, error) {
 	return connectChat(bearer)
+}
+
+type Chat interface {
+	Chats(userID string) <-chan *WSChat
+	Invites(userID string) <-chan *WSInvite
+}
+
+type chat struct {
+	ws     *turnpike.Client
+	bearer string
+	hello  map[string]interface{}
+}
+
+func topic(name string) string { return chatNamespace + "." + name }
+
+func connectChat(bearer string) (Chat, error) {
+	ws, err := turnpike.NewWebsocketClient(turnpike.JSON, chatRoot, nil, nil, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	ws.Auth = map[string]turnpike.AuthFunc{
+		"ticket": turnpike.NewTicketAuthenticator(bearer),
+	}
+	hello, err := ws.JoinRealm(topic("ifunny"), nil)
+	if err != nil {
+		panic(err)
+	}
+
+	return &chat{ws, bearer, hello}, nil
+}
+
+func (chat *chat) Chats(userID string) <-chan *WSChat {
+	result := make(chan *WSChat)
+	chat.ws.Subscribe(topic("user."+userID+".chats"), nil, func(_ []interface{}, kwargs map[string]interface{}) {
+		if kwargs["chats"] == nil {
+			return
+		}
+
+		for _, chatRaw := range kwargs["chats"].([]interface{}) {
+			wsChat := new(WSChat)
+			mapstructure.Decode(chatRaw, wsChat)
+			result <- wsChat
+		}
+	})
+
+	return result
+}
+
+func (chat *chat) Invites(userID string) <-chan *WSInvite {
+	result := make(chan *WSInvite)
+	chat.ws.Subscribe(topic("user."+userID+".invites"), nil, func(_ []interface{}, kwargs map[string]interface{}) {
+		if kwargs["invites"] == nil {
+			return
+		}
+
+		for _, invRaw := range kwargs["invites"].([]interface{}) {
+			fmt.Printf("invite: %+v\n", invRaw)
+
+			wsInvite := new(WSInvite)
+			mapstructure.Decode(invRaw, wsInvite)
+			result <- wsInvite
+		}
+	})
+
+	return result
 }
