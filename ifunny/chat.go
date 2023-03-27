c539d61b1901f24e4f76f80fc427fbd9b080f8c2
Eva <mail@gastrodon.io>
2023-03-27 10:26:16 -0400

    Add chats api code

--- ifunny/chat.go
+++ ifunny/chat.go
@@ -1,1 +1,24 @@
 package ifunny
+
+const (
+	CHATS_TRENDING = "/chats/trending"
+)
+
+type APIChannel struct {
+	Name        string `json:"name"`
+	Title       string `json:"title"`
+	Type        int    `json:"type"`
+	Description string `json:"description"`
+	JoinState   int    `json:"join_state"`
+}
+
+func (client *Client) Channels(path string) ([]APIChannel, error) {
+	response := new(struct {
+		Data struct {
+			Channels []APIChannel `json:"channels"`
+		} `json:"data"`
+	})
+
+	err := client.apiRequest(response, "GET", path, nil)
+	return response.Data.Channels, err
+}
