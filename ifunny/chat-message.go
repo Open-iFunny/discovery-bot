07d50d1dcb0a3a54dd9b9cbcbf0e026c8fd09100
Eva <mail@gastrodon.io>
2023-03-31 09:43:16 -0400

    WIP generic event subscription

--- ifunny/chat-message.go
+++ ifunny/chat-message.go
@@ -1,5 +1,9 @@
 package ifunny
 
+import (
+	"github.com/gastrodon/popplio/ifunny/compose"
+)
+
 const (
 	UNK_0 messageType = iota
 	MESSAGE
@@ -10,11 +14,11 @@ const (
 
 type messageType int
 
-type ChatMessage struct {
+type ChatEvent struct {
 	ID   string `json:"id"`
 	Text string `json:"text"`
 
-	Type   messageType `json:"type"` // 1 = message, ???
+	Type   messageType `json:"type"`
 	Status int         `json:"status"`
 	PubAt  float64     `json:"pub_at"`
 
@@ -24,4 +28,21 @@ type ChatMessage struct {
 		IsVerified bool   `json:"is_verified"`
 		LastSeenAt int64  `json:"last_seen_at"`
 	} `json:"user"`
+}
+
+func (chat *Chat) OnChanneEvent(channel string, handle func(event *ChatEvent) error) (func(), error) {
+	return chat.Subscribe(compose.EventsIn(channel), func(eventType int, kwargs map[string]interface{}) error {
+		log := chat.client.log.WithField("event_type", eventType)
+
+		if kwargs["message"] == nil {
+			log.WithField("kwargs", kwargs).Warn("channel event message is nil")
+		}
+
+		message := new(ChatEvent)
+		if err := jsonDecode(kwargs["message"], message); err != nil {
+			return err
+		}
+
+		return handle(message)
+	})
 }
--- ifunny/event.go
+++ ifunny/event.go
@@ -5,5 +5,6 @@ type EventHandler func(eventType int, kwargs map[string]interface{}) error
 const (
 	EVENT_UNKNOWN = -1
 	EVENT_JOIN    = 100
+	EVENT_MESSAGE = 200
 	EVENT_INVITED = 300
 )
